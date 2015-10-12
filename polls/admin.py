219b29359f6b1ae0f8a159a0f01ca73dac4b2b7a
nalorakq8 <yousefmon@gmail.com>
2015-10-12 03:00:08 +0300

    added the inline admin

--- polls/admin.py
+++ polls/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from .models import Poll , Choice , Response
+class ChoiceInline(admin.TabularInline):
+	model = Choice
 class PollAdmin (admin. ModelAdmin ):
 	list_display = ('name' ,'category', 'question')
+	inlines = [ChoiceInline,]
 
 admin.site.register(Poll , PollAdmin)
 admin.site.register(Choice)
