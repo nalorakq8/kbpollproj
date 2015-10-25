fe4bdf7f50654d3d3bc768e1b1a90b0d32ba9a60
nalorakq8 <yousefmon@gmail.com>
2015-10-25 23:42:50 +0300

    challenge 3.5 part 5

--- polls/views.py
+++ polls/views.py
@@ -42,8 +42,8 @@ class Survey(ListView):
 	model = Survey
 	template_name = "poll_list.html"
 	context_object_name = "polls"
-	queryset = Survey.objects.filter(questions__choice__label__iexact = 'football')
-
+	#queryset = Survey.objects.filter(questions__choice__label__iexact = 'football')
+	queryset = Survey.objects.filter(questions__choice__isnull = True)
 		
 	#we can use get_object to return single object the view will display
 	#we can use get_context_data to return list of objects
