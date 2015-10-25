d214ee67f2c8a6ab94b59afe7e7cb03d863ead2b
nalorakq8 <yousefmon@gmail.com>
2015-10-25 23:25:03 +0300

    challenge 3.5 part 2 and 3

--- polls/admin.py
+++ polls/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Poll , Choice , Response , UserProfile
+from .models import Poll , Choice , Response , UserProfile , Survey
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import User
 class ChoiceInline(admin.StackedInline):
@@ -19,14 +19,22 @@ class ChoiceAdmin (admin. ModelAdmin ):
 	
 class ResponseAdmin (admin. ModelAdmin ):
 	list_display = ('poll_name',"poll_category" , 'choice_label', 'comment')
+
 class UserAdmin(UserAdmin):
 	inlines = (UserProfileInline,)
+
 class UserProfileAdmin(admin.ModelAdmin):
 	list_display = ('user' , 'gender' , 'bio' , 'location' , 'phone')
+
+class SurveyAdmin(admin.ModelAdmin):
+	list_display = ('name' ,)
+#Challenge 3.5 part 3
+#we use through in many to many relationship to specify the intermediat table and its used when you want to associate extra data
 
 admin.site.register(Poll , PollAdmin)
 admin.site.register(Choice , ChoiceAdmin)
 admin.site.register(Response , ResponseAdmin)
+admin.site.register(Survey , SurveyAdmin)
 admin.site.unregister(User)
 admin.site.register(User, UserAdmin)
-# Register your models here.
+
